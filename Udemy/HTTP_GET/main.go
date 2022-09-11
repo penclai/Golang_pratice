d260a196755e8a9c4af3744ddb3ecc712245b61b
penclai <61202928+penclai@users.noreply.github.com>
2022-09-11 23:51:53 +0100

    update

--- Udemy/HTTP_GET/main.go
+++ Udemy/HTTP_GET/main.go
@@ -12,17 +12,17 @@ import (
 func main() {
 
 	args := os.Args
-	if len(args)<2 {
-		fmt.Printf("Usage: ./hello-world <url>\n")
+	if len(args) < 2 {
+		fmt.Printf("Usage: ./http-get <url>\n")
 		os.Exit(1)
 	}
-	
-	if _, err := url.ParseRequestURI(args[1]); err!= nil{
+
+	if _, err := url.ParseRequestURI(args[1]); err != nil {
 		fmt.Printf("URL is in a invalid format: %s\n", args[1])
 		os.Exit(1)
 	}
 
-	response, err := http.Get(args[1]);
+	response, err := http.Get(args[1])
 
 	if err != nil {
 		log.Fatal(err)
---
+++ Udemy/HTTP_JSON_Parse/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+)
+
+// {"page":"words","input":"","words":["word1"]}
+
+type Words struct{
+	Page string `json:"page"`
+	Input string `json:"input"`
+	Words []string `json:"words"`
+}
+
+func main() {
+
+	args := os.Args
+	if len(args)<2 {
+		fmt.Printf("Usage: ./http-get <url>\n")
+		os.Exit(1)
+	}
+	
+	if _, err := url.ParseRequestURI(args[1]); err!= nil{
+		fmt.Printf("URL is in a invalid format: %s\n", args[1])
+		os.Exit(1)
+	}
+
+	response, err := http.Get(args[1]);
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if response.StatusCode != 200 {
+		fmt.Printf("Invalid output (HTTP Code %d): %s\n", response.StatusCode, body)
+		os.Exit(1)
+	}
+
+	var words Words
+
+	err = json.Unmarshal(body,&words)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("JSON Parsed\nPage: %s\nWords: %v\n", words.Page, strings.Join(words.Words, ", "))
+}
